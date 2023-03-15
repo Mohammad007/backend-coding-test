c1bb16ed55ef2cd18ef9890d725f56994582b23d
Bilal <bilalmalik1561@gmail.com>
2023-03-15 12:11:59 -0700

    task complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const logger = require('./logger');
 const app = express();
 const port = 8010;
 
@@ -12,10 +13,25 @@ const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 
+// Capture 500 errors
+app.use((err,req,res,next) => {
+    res.status(500).send('Could not perform the project!');
+    logger.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+})
+    
+// Capture 404 erors
+app.use((req,res,next) => {
+    res.status(404).send("PAGE NOT FOUND");
+    logger.error(`400 || ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+})
+
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    app.listen(port, () => {
+         console.log(`App started and listening on port ${port}`)
+         logger.info(`Server started and running on http://localhost:${port}`)
+    });
 });
